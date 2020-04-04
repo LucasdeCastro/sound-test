2b05a0c5a0d8c9d7a1552a4f96fa478e720cade0
Lucas Castro <lucascastro102@gmail.com>
2020-04-04 20:33:54 -0300

    Add Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+
+ReactGA.initialize('UA-146904576-1');
 
 ReactDOM.render(
   <React.StrictMode>
