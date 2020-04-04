89c4866726abe64cd922ecde2538b1258d7ac2b6
Lucas Castro <lucascastro102@gmail.com>
2020-04-04 20:16:33 -0300

    Updates

---
+++ precache-manifest.f4119797b683cc5dd01189aa332517c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6658a5cbbf3a9848d9c97bb7758ccb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "768f97846f039cf9542f",
+    "url": "/static/css/main.d1ef5c67.chunk.css"
+  },
+  {
+    "revision": "7f030ae87e62e04b938d",
+    "url": "/static/js/2.c64b32ee.chunk.js"
+  },
+  {
+    "revision": "79bf6b85e02dc477941d6f9aa2dad84c",
+    "url": "/static/js/2.c64b32ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768f97846f039cf9542f",
+    "url": "/static/js/main.8dba748a.chunk.js"
+  },
+  {
+    "revision": "ceeefc035fef2d51680f",
+    "url": "/static/js/runtime-main.4eb93fbe.js"
+  }
+]);
